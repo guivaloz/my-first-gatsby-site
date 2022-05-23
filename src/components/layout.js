b8a77c3076f1fffdeedfb4beed910f8f16ac93db
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-22 19:10:52 -0500

    Part 5

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,39 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { container, siteTitle, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }`
+  )
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>Home</Link>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>About</Link>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
@@ -22,6 +43,7 @@ const Layout = ({ pageTitle, children }) => {
       </main>
     </div>
   )
+
 }
 
 export default Layout
